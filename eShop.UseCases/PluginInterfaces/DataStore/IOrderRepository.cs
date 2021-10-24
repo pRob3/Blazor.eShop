060642be1ffeae6ea132922c18b9454b29ff57b1
pRob3 <bubblan@slaf.se>
2021-10-24 09:04:19 +0200

    Added Order Repository

---
+++ eShop.UseCases/PluginInterfaces/DataStore/IOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using eShop.CoreBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.PluginInterfaces.DataStore
+{
+    public interface IOrderRepository
+    {
+        Order GetOrder(int id);
+        Order GetOrderByUniqueId(string uniqueId);
+        int CreateOrder(Order order);
+        void UpdateOrder(Order order);
+        IEnumerable<Order> GetOrders();
+        IEnumerable<Order> GetOutstandingOrders();
+        IEnumerable<Order> GetProcessedOrders();
+
+        IEnumerable<OrderLineItem> GetLineItemsByOrderId(int orderId);
+    }
+}
